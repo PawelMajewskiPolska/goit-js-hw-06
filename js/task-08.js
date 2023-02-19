a8d1ebbce9933fbb3ccede0198bf93a6be9c64cf
PawelMajewskiPolska <114810121+PawelMajewskiPolska@users.noreply.github.com>
2023-02-19 20:11:21 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(eve) {
+	eve.preventDefault();
+	const {
+		elements: { email, password },
+	} = eve.currentTarget;
+
+	if (email.value === "" || password.value === "") {
+		return alert("Uzupełnij wszystkie pola :-)");
+	}
+
+	console.log(`Login: ${email.value}, Password: ${password.value}`);
+	eve.currentTarget.reset();
+}

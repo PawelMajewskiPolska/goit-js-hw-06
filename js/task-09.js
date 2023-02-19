33fad8eb0f216acdf86f65d3c7490d03e65cc1e9
PawelMajewskiPolska <114810121+PawelMajewskiPolska@users.noreply.github.com>
2023-02-19 21:16:06 +0100

    Task - 09 has finished :-)

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const showBackgroundColor = document.querySelector(".widget p");
+const changeColorInput = document.querySelector(".change-color");
+const bodyColor = document.querySelector("body");
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	const result = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	/* console.log(result); */
+	showBackgroundColor.innerHTML = `Background color: ${result}`;
+	bodyColor.setAttribute("style", `background-color:${result}`);
 }
+
+changeColorInput.addEventListener("click", getRandomHexColor);
